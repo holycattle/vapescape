283ef65985eb2d18eee963a15eae3b5c1774224a
Kevin Velasco <me@kvnvelasco.com>
2015-05-27 13:49:51 -0400

    production version

---
+++ backend/opencart/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kvnvelasco.com:16000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://kvnvelasco.com:16000/');
+
+// DIR
+define('DIR_APPLICATION', '/root/vapescape/catalog/');
+define('DIR_SYSTEM', '/root/vapescape/system/');
+define('DIR_LANGUAGE', '/root/vapescape/catalog/language/');
+define('DIR_TEMPLATE', '/root/vapescape/catalog/view/theme/');
+define('DIR_CONFIG', '/root/vapescape/system/config/');
+define('DIR_IMAGE', '/root/vapescape/image/');
+define('DIR_CACHE', '/root/vapescape/system/cache/');
+define('DIR_DOWNLOAD', '/root/vapescape/system/download/');
+define('DIR_UPLOAD', '/root/vapescape/system/upload/');
+define('DIR_MODIFICATION', '/root/vapescape/system/modification/');
+define('DIR_LOGS', '/root/vapescape/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db');
+define('DB_USERNAME', 'vape');
+define('DB_PASSWORD', 'vapescape');
+define('DB_DATABASE', 'vapescape');
+define('DB_PREFIX', 'oc_');
