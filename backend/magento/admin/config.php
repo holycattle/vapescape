842993d70da0933a1de4341c2c0adecea9da8433
Kevin Velasco <Velasco.kevinR@gmail.com>
2015-05-27 20:40:21 +0800

    Initial commit to repo

---
+++ backend/magento/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.59.103:16000/admin/');
+define('HTTP_CATALOG', 'http://192.168.59.103:16000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.59.103:16000/admin/');
+define('HTTPS_CATALOG', 'http://192.168.59.103:16000/');
+
+// DIR
+define('DIR_APPLICATION', '/root/vapescape/admin/');
+define('DIR_SYSTEM', '/root/vapescape/system/');
+define('DIR_LANGUAGE', '/root/vapescape/admin/language/');
+define('DIR_TEMPLATE', '/root/vapescape/admin/view/template/');
+define('DIR_CONFIG', '/root/vapescape/system/config/');
+define('DIR_IMAGE', '/root/vapescape/image/');
+define('DIR_CACHE', '/root/vapescape/system/cache/');
+define('DIR_DOWNLOAD', '/root/vapescape/system/download/');
+define('DIR_UPLOAD', '/root/vapescape/system/upload/');
+define('DIR_LOGS', '/root/vapescape/system/logs/');
+define('DIR_MODIFICATION', '/root/vapescape/system/modification/');
+define('DIR_CATALOG', '/root/vapescape/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db');
+define('DB_USERNAME', 'vape');
+define('DB_PASSWORD', 'vapescape');
+define('DB_DATABASE', 'vapescape');
+define('DB_PREFIX', 'vape_');
