d6623dc59d73308582f2af58cb4bdb32cb8ced13
Kevin Velasco <Velasco.kevinR@gmail.com>
2015-06-28 13:29:15 +0800

    production sync test

---
+++ backend/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://192.168.59.103:8002/admin/');
+define('HTTP_CATALOG', 'http://192.168.59.103:8002/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://192.168.59.103:8002/admin/');
+define('HTTPS_CATALOG', 'http://192.168.59.103:8002/');
+
+// DIR
+define('DIR_APPLICATION', '/vapescape/admin/');
+define('DIR_SYSTEM', '/vapescape/system/');
+define('DIR_LANGUAGE', '/vapescape/admin/language/');
+define('DIR_TEMPLATE', '/vapescape/admin/view/template/');
+define('DIR_CONFIG', '/vapescape/system/config/');
+define('DIR_IMAGE', '/vapescape/image/');
+define('DIR_CACHE', '/vapescape/system/cache/');
+define('DIR_DOWNLOAD', '/vapescape/system/download/');
+define('DIR_UPLOAD', '/vapescape/system/upload/');
+define('DIR_LOGS', '/vapescape/system/logs/');
+define('DIR_MODIFICATION', '/vapescape/system/modification/');
+define('DIR_CATALOG', '/vapescape/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', getenv("DBHOST"));
+define('DB_USERNAME', 'vape');
+define('DB_PASSWORD', getenv("DB_PASS"));
+define('DB_DATABASE', 'vapescape');
+define('DB_PREFIX', 'oc_');
