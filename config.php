8311a8508ecfccc35fb15828331c23d6ea4fbee4
Kevin Velasco <Velasco.kevinR@gmail.com>
2015-07-21 13:42:31 +0800

    it's ready for you joniel

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', getenv('HTTP_SERVER'));
+
+// HTTPS
+define('HTTPS_SERVER', getenv('HTTPS_SERVER'));
+
+// DIR
+define('DIR_APPLICATION', '/vapescape/catalog/');
+define('DIR_SYSTEM', '/vapescape/system/');
+define('DIR_LANGUAGE', '/vapescape/catalog/language/');
+define('DIR_TEMPLATE', '/vapescape/catalog/view/theme/');
+define('DIR_CONFIG', '/vapescape/system/config/');
+define('DIR_IMAGE', '/vapescape/image/');
+define('DIR_CACHE', '/vapescape/system/cache/');
+define('DIR_DOWNLOAD', '/vapescape/system/download/');
+define('DIR_UPLOAD', '/vapescape/system/upload/');
+define('DIR_MODIFICATION', '/vapescape/system/modification/');
+define('DIR_LOGS', '/vapescape/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', getenv('MYSQL_PORT_3306_TCP_ADDR'));
+define('DB_USERNAME', getenv('MYSQL_VAPE_USERNAME'));
+define('DB_PASSWORD', getenv('MYSQL_VAPE_PASSWORD'));
+define('DB_DATABASE', getenv('MYSQL_VAPE_DATABASE'));
+define('DB_PREFIX', getenv('MYSQL_VAPE_DATABASE_PREFIX'));
